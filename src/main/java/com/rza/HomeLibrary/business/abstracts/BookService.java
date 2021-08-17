d41ad58f3543f336eaa5d942815285b1beb2767b
rza35 <riza_tire@hotmail.com>
2021-08-18 00:50:48 +0300

    Ekleme

--- src/main/java/com/rza/HomeLibrary/business/abstracts/BookService.java
+++ src/main/java/com/rza/HomeLibrary/business/abstracts/BookService.java
@@ -3,10 +3,19 @@ package com.rza.HomeLibrary.business.abstracts;
 import com.rza.HomeLibrary.core.utilities.results.DataResult;
 import com.rza.HomeLibrary.core.utilities.results.Result;
 import com.rza.HomeLibrary.entities.concretes.Book;
+import com.rza.HomeLibrary.entities.dtos.BookDetailDto;
 
 import java.util.List;
 
 public interface BookService {
     Result add(Book book);
     DataResult<List<Book>> getAll();
+    DataResult<List<Book>> findBookByBookName(String bookName);
+    DataResult<Book> findBookById(int id);
+    DataResult<List<Book>> findBookByCategory_CategoryId(int categoryId);
+
+    DataResult<Book> findBookByName(String bookName);
+
+    DataResult<List<BookDetailDto>> getBookDetail();
+
 }
