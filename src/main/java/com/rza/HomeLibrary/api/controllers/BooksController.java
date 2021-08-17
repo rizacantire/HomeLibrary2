fe0a1df2426672e568c3ff36e81692883bcdeaef
rza35 <riza_tire@hotmail.com>
2021-08-18 00:51:09 +0300

    Ekleme

--- src/main/java/com/rza/HomeLibrary/api/controllers/BooksController.java
+++ src/main/java/com/rza/HomeLibrary/api/controllers/BooksController.java
@@ -4,6 +4,7 @@ import com.rza.HomeLibrary.business.abstracts.BookService;
 import com.rza.HomeLibrary.core.utilities.results.DataResult;
 import com.rza.HomeLibrary.core.utilities.results.Result;
 import com.rza.HomeLibrary.entities.concretes.Book;
+import com.rza.HomeLibrary.entities.dtos.BookDetailDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/books/")
+@CrossOrigin
 public class BooksController {
 
     private BookService bookService;
@@ -28,6 +30,32 @@ public class BooksController {
     public Result add(@RequestBody Book book){
         return this.bookService.add(book);
     }
+
+    @PostMapping("findBook")
+    public Result findBook(@RequestBody String bookName){
+        return this.bookService.findBookByBookName(bookName);
+    }
+
+    @PostMapping("findById")
+    public Result findById(@RequestBody int id){
+        return this.bookService.findBookById(id);
+    }
+
+    @GetMapping("findCategory")
+    public Result findCategory(@RequestParam int id){
+        return this.bookService.findBookByCategory_CategoryId(id);
+    }
+
+    @GetMapping("getDetail")
+    public DataResult<List<BookDetailDto>> getDetail(){
+        return this.bookService.getBookDetail();
+    }
+
+    @GetMapping("findName")
+    public DataResult<Book> findName(@RequestParam String name){
+        return this.bookService.findBookByName(name);
+    }
+
 
 
 }
